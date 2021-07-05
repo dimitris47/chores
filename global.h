f9157e038a755f411934de9d6b130dd49d645ce9
dimitris47 <dimitrisinbox@gmail.com>
2021-07-05 13:07:20 +0300

    add icon attributions & license in sources

--- global.h
+++ global.h
@@ -3,13 +3,16 @@
 
 #include <QString>
 
+const QString br = "<br/>";
 const QString APPNAME("Chores");
-const QString VERSION("2.3");
+const QString VERSION("2.3.1");
 const QString CONTRIBUTORS("Dimitris Psathas");
-const QString APPINFO("Program created by Dimitris Psathas<br/><br/>"
-                      "Written in C++, built against Qt 5.15.2<br/><br/>"
-                      "Published under the GNU General Public License v3.0<br/>"
-                      "Qt libraries used under (L)GPL3<br/><br/>"
+const QString APPINFO("Program created by Dimitris Psathas" + br.repeated(2) +
+                      "Written in C++, built against Qt 5.15.2" + br.repeated(2) +
+                      "Published under the GNU General Public License v3.0" + br +
+                      "Qt libraries used under (L)GPL3" + br +
+                      "Application icon by <a href='https://icons8.com'>icons8.com</a>" + br +
+                      "In-app icons from Yaru & Oxygen themes" + br.repeated(2) +
                       "&copy; 2020-2021 Dimitris Psathas");
 
 #endif // GLOBAL_H
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2020-2021 Dimitris Psathas <dimitrisinbox@gmail.com>
+ *
+ * This file is part of Chores.
+ *
+ * Chores is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License  as  published by  the  Free Software
+ * Foundation,  either version 3 of the License,  or (at your option)  any later
+ * version.
+ *
+ * Chores is distributed in the hope that it will be useful,  but  WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the  GNU General Public License  for more details.
+ *
+ * You should have received a copy of the  GNU General Public License along with
+ * Chores. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "combo.h"
@@ -147,7 +166,7 @@ void MainWindow::on_actionExport_triggered() {
 
 void MainWindow::on_actionAbout_triggered() {
     QMessageBox::about(this, tr("Program Info"),
-                       (QApplication::applicationName() + " " + QApplication::applicationVersion() + "<br/><br/>" +
+                       (QApplication::applicationName() + " " + QApplication::applicationVersion() + br.repeated(2) +
                         APPINFO));
 }
 
